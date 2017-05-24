edd634d1c9166cfbffca5d8158f819f23ec9dcf0
BK <bkam03@yahoo.com>
2017-05-23 14:21:13 -1000

    code cleaned up

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -2,13 +2,11 @@ exports.largestPrimeFactor = function(n){
 
   function primeFinder( num ) {
     var primeNumber = num;
-    //for loop that goes until i reaches n, or a prime factor (not 1) is found
 
+    //for loop that goes until i reaches n, or a prime factor (not 1) is found
     for( var i = 2; i < num; i++ ) {
-        //modulus n by each number, and it should == 0 if it is a largestPrimeFactor
-      console.log("cycle" + i );
+        //modulus n by each number, and it should == 0 if it is largestPrimeFactor
       if( num % i === 0 ) {
-        console.log( i );
         //if modulus comes out 0, then recurse
         primeNumber = primeFinder( primeNumber / i );
         break;
@@ -17,5 +15,6 @@ exports.largestPrimeFactor = function(n){
     }
     return primeNumber;
   }
+  //start recursion cycle
   return primeFinder( n );
 };
