fef85ff078901c985901a9a09087fb4b85ea3583
BK <bkam03@yahoo.com>
2017-05-23 14:19:23 -1000

    largestPrimeFactor function works by recursion

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,21 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
 
-  return primeNumber;
+  function primeFinder( num ) {
+    var primeNumber = num;
+    //for loop that goes until i reaches n, or a prime factor (not 1) is found
+
+    for( var i = 2; i < num; i++ ) {
+        //modulus n by each number, and it should == 0 if it is a largestPrimeFactor
+      console.log("cycle" + i );
+      if( num % i === 0 ) {
+        console.log( i );
+        //if modulus comes out 0, then recurse
+        primeNumber = primeFinder( primeNumber / i );
+        break;
+      }
+      //either return n if no factor found, or found factor
+    }
+    return primeNumber;
+  }
+  return primeFinder( n );
 };
